bf4d11d506ff160f0904079ab9b86f99cf06dbc4
Erik Torres <erik.torres@lifullconnect.com>
2023-08-07 16:30:32 +0200

    :art: Replace iron with constructor

--- apps/realworld-http/src/main/scala/realworld/adapter/persistence/row/UserRow.scala
+++ apps/realworld-http/src/main/scala/realworld/adapter/persistence/row/UserRow.scala
@@ -2,7 +2,7 @@ package es.eriktorr
 package realworld.adapter.persistence.row
 
 import realworld.domain.model.Password.Format
-import realworld.domain.model.User.{Token, Username}
+import realworld.domain.model.User.Username
 import realworld.domain.model.{Email, Password, User, UserWithPassword}
 import realworld.shared.Secret
 import realworld.shared.data.refined.StringExtensions.toUri
@@ -12,7 +12,6 @@ import cats.Semigroup
 import cats.data.NonEmptyChain
 import cats.data.Validated.{Invalid, Valid}
 import cats.implicits.{catsSyntaxTuple3Semigroupal, toTraverseOps}
-import io.github.arainko.ducktape.*
 import org.tpolecat.typename.TypeName
 
 final case class UserRow(
@@ -28,16 +27,7 @@ object UserRow:
   extension (userRow: UserRow)
     def toUser: AllErrorsOr[User] =
       (Email.from(userRow.email), Username.from(userRow.username), userRow.image.traverse(_.toUri))
-        .mapN { case (email, username, image) =>
-          userRow
-            .into[User]
-            .transform(
-              Field.const(_.email, email),
-              Field.const(_.token, Option.empty[Token]),
-              Field.const(_.username, username),
-              Field.const(_.image, image),
-            )
-        }
+        .mapN { case (email, username, image) => User(email, None, username, userRow.bio, image) }
 
     def toUserWithPassword[A <: Format, B <: UserWithPassword[A]](using
         typeNameA: TypeName[A],
