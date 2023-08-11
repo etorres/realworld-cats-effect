013d6aec23e7fa309e9e50250f020ce7a4bca37f
Erik Torres <erik.torres@lifullconnect.com>
2023-08-11 21:17:19 +0200

    :construction: Save WiP

--- apps/realworld-http/src/main/scala/realworld/adapter/persistence/PostgresArticlesRepository.scala
+++ apps/realworld-http/src/main/scala/realworld/adapter/persistence/PostgresArticlesRepository.scala
@@ -9,10 +9,13 @@ import realworld.adapter.persistence.row.ArticleRow
 import realworld.domain.model.{Article, UserId}
 import realworld.domain.service.{ArticlesFilters, ArticlesRepository, Pagination}
 
+import cats.data.NonEmptyList
 import cats.effect.IO
+import doobie.Fragments.whereAnd
 import doobie.hikari.HikariTransactor
 import doobie.implicits.*
-import doobie.postgres.implicits.given
+import doobie.postgres.implicits.JavaTimeLocalDateTimeMeta
+import doobie.util.fragment.Fragment
 
 final class PostgresArticlesRepository(transactor: HikariTransactor[IO]) extends ArticlesRepository:
   override def findArticlesBy(
@@ -20,33 +23,33 @@ final class PostgresArticlesRepository(transactor: HikariTransactor[IO]) extends
       pagination: Pagination,
       userId: UserId,
   ): IO[List[Article]] = for
-    articleRows <- sql"""SELECT
-                        |  articles.article_id,
-                        |  articles.slug,
-                        |  articles.title,
-                        |  articles.description,
-                        |  articles.body,
-                        |  articles.created_at,
-                        |  articles.updated_at,
-                        |  articles.author_id
-                        |FROM articles
-                        |LEFT JOIN users authors ON authors.user_id = articles.author_id
-                        |LEFT JOIN favorites ON favorites.article_id = articles.article_id
-                        |LEFT JOIN users ON users.user_id = favorites.profile_id
-                        |LEFT JOIN tags ON tags.article_id = articles.article_id
-                        |${whereFrom(filters).getOrElse("")}
-                        |GROUP BY
-                        |  articles.article_id,
-                        |  articles.slug,
-                        |  articles.title,
-                        |  articles.description,
-                        |  articles.body,
-                        |  articles.created_at,
-                        |  articles.updated_at,
-                        |  articles.author_id
-                        |ORDER BY articles.created_at DESC
-                        |LIMIT ${pagination.limit}
-                        |OFFSET ${pagination.offset}""".stripMargin
+    articleRows <- fr"""SELECT
+                       |  articles.article_id,
+                       |  articles.slug,
+                       |  articles.title,
+                       |  articles.description,
+                       |  articles.body,
+                       |  articles.created_at,
+                       |  articles.updated_at,
+                       |  articles.author_id
+                       |FROM articles
+                       |LEFT JOIN users authors ON authors.user_id = articles.author_id
+                       |LEFT JOIN favorites ON favorites.article_id = articles.article_id
+                       |LEFT JOIN users ON users.user_id = favorites.profile_id
+                       |LEFT JOIN tags ON tags.article_id = articles.article_id
+                       |${whereAnd(whereFrom(filters))}
+                       |GROUP BY
+                       |  articles.article_id,
+                       |  articles.slug,
+                       |  articles.title,
+                       |  articles.description,
+                       |  articles.body,
+                       |  articles.created_at,
+                       |  articles.updated_at,
+                       |  articles.author_id
+                       |ORDER BY articles.created_at DESC
+                       |LIMIT ${pagination.limit}
+                       |OFFSET ${pagination.offset}""".stripMargin
       .query[ArticleRow]
       .to[List]
       .transact(transactor)
@@ -54,11 +57,13 @@ final class PostgresArticlesRepository(transactor: HikariTransactor[IO]) extends
     result <- IO.pure(List.empty) // TODO
   yield result
 
-  private def whereFrom(filters: ArticlesFilters): Option[String] =
-    (
-      filters.author.map(x => s"authors.username = $x"),
-      filters.favorited.map(x => s"favorites.username = $x"),
-      filters.tag.map(x => s"tags.tag = $x"),
-    ).toList.collect { case Some(value) => value } match
-      case ::(head, next) => Some(s"WHERE $head ${next.mkString("AND ")}")
-      case Nil => None
+  private def whereFrom(filters: ArticlesFilters): NonEmptyList[Fragment] =
+    NonEmptyList
+      .fromList(
+        (
+          filters.author.map(x => fr"authors.username = $x"),
+          filters.favorited.map(x => fr"favorites.username = $x"),
+          filters.tag.map(x => fr"tags.tag = $x"),
+        ).toList.collect { case Some(value) => value },
+      )
+      .getOrElse(NonEmptyList.one(fr"true"))
